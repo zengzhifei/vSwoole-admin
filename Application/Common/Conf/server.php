681c2e3cfbebbf18fbe0efada132f6ea83894d4f
zengzhifei <zengzhifei@ctvit.com.cn>
2018-05-21 11:28:19 +0800

    删除没用文件

--- Application/Common/Conf/server.php
+++ Application/Common/Conf/server.php
@@ -11,49 +11,49 @@ return [
     //WebSocket
     'WebSocket' => [
         //获取归档号
-        'Get_Ranges'        => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/getRanges',
+        'Get_Ranges'        => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/getRanges',
         //获取配置
-        'Get_Configs'       => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/getConfigs',
+        'Get_Configs'       => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/getConfigs',
         //获取服务器IP
-        'Get_Servers'       => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/getServerList',
+        'Get_Servers'       => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/getServerList',
         //清理服务器IP
-        'Clear_Servers'     => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/clearServers',
+        'Clear_Servers'     => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/clearServers',
         //获取服务器在线人数
-        'Get_Server_Online' => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/getServerOnline',
+        'Get_Server_Online' => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/getServerOnline',
         //重载服务器
-        'Reload_Server'     => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/reload',
+        'Reload_Server'     => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/reload',
         //关闭服务器
-        'ShutDown_Server'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/shutdown',
+        'ShutDown_Server'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/shutdown',
         //获取归档在线人数
-        'Get_Range_Online'  => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/getRangeOnline',
+        'Get_Range_Online'  => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/getRangeOnline',
         //消息推送
-        'Push_Message'      => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/send',
+        'Push_Message'      => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/send',
         //关闭连接
-        'Close_Connect'     => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/close',
+        'Close_Connect'     => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/close',
         //设置配置
-        'Set_Configs'       => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=WebSocket/config',
+        'Set_Configs'       => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=WebSocket/config',
     ],
     //Crontab
     'Crontab'   => [
         //获取服务器IP
-        'Get_Servers'     => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/getServerList',
+        'Get_Servers'     => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/getServerList',
         //重载服务器
-        'Reload_Server'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/reload',
+        'Reload_Server'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/reload',
         //关闭服务器
-        'ShutDown_Server' => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/shutdown',
+        'ShutDown_Server' => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/shutdown',
         //清理服务器IP
-        'Clear_Servers'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/clearServers',
+        'Clear_Servers'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/clearServers',
         //添加任务
-        'Add_Task'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/addTask',
+        'Add_Task'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/addTask',
         //获取任务列表
-        'Get_Task_List'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/getTaskList',
+        'Get_Task_List'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/getTaskList',
         //获取任务
-        'Get_Task'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/getTask',
+        'Get_Task'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/getTask',
         //开始任务
-        'Start_Task'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/startTask',
+        'Start_Task'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/startTask',
         //暂停任务
-        'Stop_Task'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/stopTask',
+        'Stop_Task'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/stopTask',
         //删除任务
-        'Delete_Task'   => 'http://192.168.31.100/demo/vSwoole/public/client.php?s=Crontab/deleteTask',
+        'Delete_Task'   => 'http://127.0.0.1/demo/vSwoole/public/client.php?s=Crontab/deleteTask',
     ]
 ];
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,6 +15,6 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'P2;4$`vLk?@RGrUomfg"/a6h|,:nsb_AiJ<B^yEc'); //加密KEY
-//define('UC_DB_DSN', 'mysql://root:720f295aa1@123.57.223.157:3306/vswoole'); // 使用Model方式调用API必须配置此项
-define('UC_DB_DSN', 'mysql://root:Passw0rd@192.168.31.186:3306/vswoole'); // 使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:720f295aa1@123.57.223.157:3306/vswoole'); // 使用Model方式调用API必须配置此项
+//define('UC_DB_DSN', 'mysql://root:Passw0rd@192.168.31.186:3306/vswoole'); // 使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'wei_'); // 数据表前缀，使用Model方式调用API必须配置此项
