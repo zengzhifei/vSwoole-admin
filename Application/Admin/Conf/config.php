3eb69b434011acdba046f5200a38a1adfb9d0280
zengzhifei <zengzhifei@outlook.com>
2018-05-27 13:07:16 +0800

    添加favicon

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -13,17 +13,17 @@
  */
 return array(
     /* 数据缓存设置 */
-    'DATA_CACHE_PREFIX'    => 'wei_', // 缓存前缀
-    'DATA_CACHE_TYPE'      => 'File', // 数据缓存类型
-    'URL_MODEL'            => 3, //URL模式
+    'DATA_CACHE_PREFIX'      => 'wei_', // 缓存前缀
+    'DATA_CACHE_TYPE'        => 'File', // 数据缓存类型
+    'URL_MODEL'              => 3, //URL模式
 
     /*生成期数ID设置*/
-    'STAGE_PREFIX' => 'Stage',
-    'COLUMN_PREFIX' => 'Column',
+    'STAGE_PREFIX'           => 'Stage',
+    'COLUMN_PREFIX'          => 'Column',
     /* 文件上传相关配置 */
-    'DOWNLOAD_UPLOAD' => array(
+    'DOWNLOAD_UPLOAD'        => array(
         'mimes'    => '', //允许上传的文件MiMe类型
-        'maxSize'  => 5*1024*1024, //上传的文件大小限制 (0-不做限制)
+        'maxSize'  => 5 * 1024 * 1024, //上传的文件大小限制 (0-不做限制)
         'exts'     => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', //允许上传的文件后缀
         'autoSub'  => true, //自动子目录保存文件
         'subName'  => '', //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
@@ -35,60 +35,60 @@ return array(
         'hash'     => false, //是否生成hash编码
         'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
     ), //下载模型上传配置（文件上传类配置）
-    'DOWNLOAD_UPLOAD_DRIVER'=>'local',
+    'DOWNLOAD_UPLOAD_DRIVER' => 'local',
 
     /* 图片上传相关配置 */
-    'PICTURE_UPLOAD' => array(
-		'mimes'    => '', //允许上传的文件MiMe类型
-		'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
-		'exts'     => 'jpg,gif,png,jpeg,txt', //允许上传的文件后缀
-		'autoSub'  => true, //自动子目录保存文件
-		'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-		'rootPath' => './Uploads/Picture/', //保存根路径
-		'savePath' => '', //保存路径
-		'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-		'saveExt'  => '', //文件保存后缀，空则使用原后缀
-		'replace'  => false, //存在同名是否覆盖
-		'hash'     => true, //是否生成hash编码
-		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
+    'PICTURE_UPLOAD'         => array(
+        'mimes'    => '', //允许上传的文件MiMe类型
+        'maxSize'  => 2 * 1024 * 1024, //上传的文件大小限制 (0-不做限制)
+        'exts'     => 'jpg,gif,png,jpeg,txt', //允许上传的文件后缀
+        'autoSub'  => true, //自动子目录保存文件
+        'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+        'rootPath' => './Uploads/Picture/', //保存根路径
+        'savePath' => '', //保存路径
+        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        'saveExt'  => '', //文件保存后缀，空则使用原后缀
+        'replace'  => false, //存在同名是否覆盖
+        'hash'     => true, //是否生成hash编码
+        'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
     ), //图片上传相关配置（文件上传类配置）
 
-    'PICTURE_UPLOAD_DRIVER'=>'local',
+    'PICTURE_UPLOAD_DRIVER' => 'local',
     //本地上传文件驱动配置
-    'UPLOAD_LOCAL_CONFIG'=>array(),
-    'UPLOAD_BCS_CONFIG'=>array(
-        'AccessKey'=>'',
-        'SecretKey'=>'',
-        'bucket'=>'',
-        'rename'=>false
+    'UPLOAD_LOCAL_CONFIG'   => array(),
+    'UPLOAD_BCS_CONFIG'     => array(
+        'AccessKey' => '',
+        'SecretKey' => '',
+        'bucket'    => '',
+        'rename'    => false
     ),
-    'UPLOAD_QINIU_CONFIG'=>array(
-        'accessKey'=>'__ODsglZwwjRJNZHAu7vtcEf-zgIxdQAY-QqVrZD',
-        'secrectKey'=>'Z9-RahGtXhKeTUYy9WCnLbQ98ZuZ_paiaoBjByKv',
-        'bucket'=>'onethinktest',
-        'domain'=>'onethinktest.u.qiniudn.com',
-        'timeout'=>3600,
+    'UPLOAD_QINIU_CONFIG'   => array(
+        'accessKey'  => '__ODsglZwwjRJNZHAu7vtcEf-zgIxdQAY-QqVrZD',
+        'secrectKey' => 'Z9-RahGtXhKeTUYy9WCnLbQ98ZuZ_paiaoBjByKv',
+        'bucket'     => 'onethinktest',
+        'domain'     => 'onethinktest.u.qiniudn.com',
+        'timeout'    => 3600,
     ),
 
 
     /* 编辑器图片上传相关配置 */
-    'EDITOR_UPLOAD' => array(
-		'mimes'    => '', //允许上传的文件MiMe类型
-		'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
-		'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
-		'autoSub'  => true, //自动子目录保存文件
-		'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-		'rootPath' => './Uploads/Editor/', //保存根路径
-		'savePath' => '', //保存路径
-		'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-		'saveExt'  => '', //文件保存后缀，空则使用原后缀
-		'replace'  => false, //存在同名是否覆盖
-		'hash'     => true, //是否生成hash编码
-		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
+    'EDITOR_UPLOAD'         => array(
+        'mimes'    => '', //允许上传的文件MiMe类型
+        'maxSize'  => 2 * 1024 * 1024, //上传的文件大小限制 (0-不做限制)
+        'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
+        'autoSub'  => true, //自动子目录保存文件
+        'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+        'rootPath' => './Uploads/Editor/', //保存根路径
+        'savePath' => '', //保存路径
+        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        'saveExt'  => '', //文件保存后缀，空则使用原后缀
+        'replace'  => false, //存在同名是否覆盖
+        'hash'     => true, //是否生成hash编码
+        'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
     ),
 
     /* 模板相关配置 */
-    'TMPL_PARSE_STRING' => array(
+    'TMPL_PARSE_STRING'     => array(
         '__STATIC__' => __ROOT__ . '/Public/static',
         '__ADDONS__' => __ROOT__ . '/Public/' . MODULE_NAME . '/Addons',
         '__IMG__'    => __ROOT__ . '/Public/' . MODULE_NAME . '/images',
@@ -97,19 +97,19 @@ return array(
     ),
 
     /* SESSION 和 COOKIE 配置 */
-    'COOKIE_PREFIX'  => 'Studio-V3_admin_', // Cookie前缀 避免冲突
-    'VAR_SESSION_ID' => 'session_id',	//修复uploadify插件无法传递session_id的bug
-    'SESSION_TYPE' => 'Redis', //session保存类型
-    'SESSION_PREFIX' => 'Studio-V3_admin', //session前缀
+    'COOKIE_PREFIX'         => 'vSwoole_admin_', // Cookie前缀 避免冲突
+    'VAR_SESSION_ID'        => 'session_id',    //修复uploadify插件无法传递session_id的bug
+    'SESSION_TYPE'          => 'Redis', //session保存类型
+    'SESSION_PREFIX'        => 'vSwoole_admin', //session前缀
 
     /* 后台错误页面模板 */
-    'TMPL_ACTION_ERROR'     =>  MODULE_PATH.'View/Public/error.html', // 默认错误跳转对应的模板文件
-    'TMPL_ACTION_SUCCESS'   =>  MODULE_PATH.'View/Public/success.html', // 默认成功跳转对应的模板文件
-    'TMPL_EXCEPTION_FILE'   =>  MODULE_PATH.'View/Public/exception.html',// 异常页面的模板文件
-    'PAGE_OFFSET' =>'15',
-    'ADMIN_LOGIN_URL'=>'http://218.241.129.64/weixin-studio/sso/getTicket?from=',//后台单点登录的地址
-    'ADMIN_LOGINOUT_URL'=>'http://218.241.129.64/appplatform/system/logoutLogin',//预留loginout地址
-    'SSO' => 'http://218.241.129.64/weixin-studio/sso/getTicket?from=',  //单点登录接口Single Sign On
-    'SSOV'  => 'http://218.241.129.64/weixin-studio/sso/verify?ticket=', //验证单点登录接口
+    'TMPL_ACTION_ERROR'     => MODULE_PATH . 'View/Public/error.html', // 默认错误跳转对应的模板文件
+    'TMPL_ACTION_SUCCESS'   => MODULE_PATH . 'View/Public/success.html', // 默认成功跳转对应的模板文件
+    'TMPL_EXCEPTION_FILE'   => MODULE_PATH . 'View/Public/exception.html',// 异常页面的模板文件
+    'PAGE_OFFSET'           => '15',
+    'ADMIN_LOGIN_URL'       => 'http://218.241.129.64/weixin-studio/sso/getTicket?from=',//后台单点登录的地址
+    'ADMIN_LOGINOUT_URL'    => 'http://218.241.129.64/appplatform/system/logoutLogin',//预留loginout地址
+    'SSO'                   => 'http://218.241.129.64/weixin-studio/sso/getTicket?from=',  //单点登录接口Single Sign On
+    'SSOV'                  => 'http://218.241.129.64/weixin-studio/sso/verify?ticket=', //验证单点登录接口
 
 );
--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -2,10 +2,10 @@
 return array(
     /* 数据库配置 */
     'DB_TYPE'          => 'mysql', // 数据库类型
-    'DB_HOST'          => '127.0.0.1', // 服务器地址
+    'DB_HOST'          => '192.168.31.186', // 服务器地址
     'DB_NAME'          => 'vswoole', // 正式数据库名
     'DB_USER'          => 'root', // 用户名
-    'DB_PWD'           => '',  // 密码
+    'DB_PWD'           => 'Passw0rd',  // 密码
     'DB_PORT'          => '3306', // 端口
     'DB_PREFIX'        => 'wei_', // 数据库表前缀
     'REDIS_HOST'       => '127.0.0.1', //redis服务器ip，多台用逗号隔开；读写分离开启时，第一台负责写，其它[随机]负责读；
